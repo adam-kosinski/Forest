2fe31d136dc606d66703e0a9e4fc788eaa860c64
Adam <adamkosinski631@gmail.com>
2020-12-21 02:46:31 -0500

    Actually setting up the heroku server

---
+++ server.js
@@ -0,0 +1,91 @@
+//SERVER SETUP --------------------------------------------------------------------------------
+
+// Dependencies
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var socketIO = require("socket.io");
+
+//app stuff
+var app = express();
+var server = http.Server(app);
+var io = socketIO(server);
+
+app.set("port", 5000);
+app.use("/static", express.static(__dirname + "/static"));
+
+// Routing
+app.get("/", function(request, response) {
+  response.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Starts the server.
+let port = process.env.PORT;
+if(port == null || port == ""){
+	port = 5000;
+}
+server.listen(port, function() {
+  console.log("Starting server on port "+port);
+});
+
+/*
+
+//STORAGE ------------------------------------------------------
+
+let players = {}; //holds Player objects, keys are player names (not socket ids)
+let id_to_name = {}; //maps socket ids to names. If a name isn't in here, player is disconnected
+
+let game = undefined; //undefined means no game currently going on
+
+
+
+// WEBSOCKET HANDLERS -------------------------------------------
+io.on("connection", function(socket) {
+
+	//when a new player joins, check if player exists. If they don't, create new player. If they do, only allow join if that player was disconnected
+	socket.on("new player", function(name, callback){
+
+		if(!players.hasOwnProperty(name)){
+			if(game != undefined){return;} //don't count spectators as players. If the game ends, they can refresh and join as a player
+
+			//new player
+			console.log("New player: " + name + " (id: " + socket.id + ")");
+			players[name] = new Player(name);
+			id_to_name[socket.id] = name;
+			callback(true); //successful
+		}
+		else if(players[name].connected){
+			console.log(name + " is a duplicate name - asking them to try another");
+			callback(false); //duplicate name, tell the client it's invalid
+		}
+		else {
+			console.log(name + " reconnected (id: " + socket.id + ")");
+			id_to_name[socket.id] = name; //add the new mapping
+			players[name].connected = true;
+			callback(true); //successful
+		}
+		io.sockets.emit("player_connection", players);
+	});
+
+	//mark player as disconnected when they leave
+	socket.on("disconnect", function(){
+		if(id_to_name.hasOwnProperty(socket.id)){
+			console.log(id_to_name[socket.id]+" disconnected (id: " + socket.id + ")");
+
+			let player = players[id_to_name[socket.id]];
+			player.connected = false;
+			delete id_to_name[socket.id];
+
+			//remove from busy_clients if there
+			let index = busy_clients.indexOf(player.name);
+			if(index != -1){
+				busy_clients.splice(index, 1);
+			}
+		}
+		io.sockets.emit("player_connection", players);
+	});
+
+	socket.on("get_state", function(callback){
+		callback(players, game); //if game is undefined, tells them no game currently happening
+	});
+*/
---
+++ static/client.js
@@ -0,0 +1,68 @@
+//SETUP ---------------------------------------------------
+
+let socket = io();
+let id; //id of the socket
+
+//CONNECTION TO SERVER -----------------------------------
+
+//send a new player message to the server, and pick name
+function registerName(){
+	//my_name declared in globals.js
+	my_name = prompt("Please enter a name (< 11 characters or display problems happen):"); //TODO: make this a GUI thing not a prompt
+	if(my_name===""){
+		registerName();
+		return;
+	}
+	if(!my_name){
+		throw new Error("Name entry canceled, leaving webpage blank");
+	}
+	
+	socket.emit("new player", my_name, function(success){
+		console.log("Name registration success:",success);
+		if(!success){
+			alert("'"+my_name+"' is taken. Please choose another");
+			my_name = undefined;
+			registerName();
+		}
+	});
+}
+
+registerName();
+
+//store the id of the connection
+socket.on("connect", function(){
+	console.log("My ID: "+socket.id);
+	id = socket.id;
+});
+
+
+//check if a game is going on
+socket.emit("get_state", function(players, game){
+	if(game){
+		game_active = true;
+		//initialize game display
+	}
+	else {
+		home_screen.style.display = "block";
+	}
+});
+
+
+
+
+//socket event handlers
+
+socket.on("player_connection", function(players){
+	//update player display on home screen
+	player_display.innerHTML = "";
+	for(let name in players){
+		if(players[name].connected){
+			let div = document.createElement("div");
+			div.id = name + "_home_screen";
+			div.textContent = name;
+			player_display.appendChild(div);
+		}
+	}
+
+	//indicate disconnected in game GUI if game active
+});
